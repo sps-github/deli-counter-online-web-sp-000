7542a7b6a29f115ce64b110e070dfaec09e50571
Sarah Peters <sarahpieters15@gmail.com>
2020-06-03 17:08:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 katz_deli = []
+
+def line(members)
+  line_array = []
+  if members.length == 0
+    puts "The line is currently empty."
+  else
+    members.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
