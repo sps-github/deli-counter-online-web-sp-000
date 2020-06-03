1bf45e85f6edffd234e3d5e4ced850c076e0b87a
Sarah Peters <sarahpieters15@gmail.com>
2020-06-03 17:10:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,8 @@ def line(members)
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
